f6e28f69b331c6d50ea60af5d7ac0a34e34abd24
Rafael Santos <rafa.05125@gmail.com>
2024-03-25 18:45:39 -0300

    Homepage design

--- blog-nextjs/src/components/footer/Footer.jsx
+++ blog-nextjs/src/components/footer/Footer.jsx
@@ -1,5 +1,12 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Rafaelse6</div>
+      <div className={styles.text}>Made by Rafael Santos</div>
+    </div>
+  );
 };
 
 export default Footer;
