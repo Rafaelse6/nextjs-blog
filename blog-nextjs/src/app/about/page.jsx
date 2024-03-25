34bbef3f51aa4c3ff7633c03c621701ab4ced8d2
Rafael Santos <rafa.05125@gmail.com>
2024-03-25 18:55:24 -0300

    About page design

--- blog-nextjs/src/app/about/page.jsx
+++ blog-nextjs/src/app/about/page.jsx
@@ -3,9 +3,35 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
-      <div className={styles.imgContainer}></div>
-      <Image src="/about.png" alt="" fill />
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          pellentesque, elit sed maximus fringilla, dolor mauris egestas est, at
+          pharetra elit dolor sed felis. Maecenas molestie ac eros a porta.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="About Image" fill className={styles.img} />
+      </div>
     </div>
   );
 };
