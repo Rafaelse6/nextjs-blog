e5a01399dc4facddd0f634d51703595b70543520
Rafael Santos <rafa.05125@gmail.com>
2024-04-03 23:58:00 -0300

    Suspense

--- blog-nextjs/src/components/postUser/postUser.jsx
+++ blog-nextjs/src/components/postUser/postUser.jsx
@@ -2,7 +2,8 @@ import styles from "./postUser.module.css";
 
 const getData = async (userId) => {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${userId}`
+    `https://jsonplaceholder.typicode.com/users/${userId}`,
+    { cache: "no-store" }
   );
 
   if (!res.ok) {
