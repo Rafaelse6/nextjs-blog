4e7f90d89dad4319621370fcf4e5b5c87dc1d18d
Rafael Santos <rafa.05125@gmail.com>
2024-04-09 10:45:18 -0300

    Login with GitHub

--- blog-nextjs/src/lib/actions.js
+++ blog-nextjs/src/lib/actions.js
@@ -3,6 +3,7 @@
 import { Post } from "./models";
 import { connectToDb } from "./utils";
 import { revalidatePath } from "next/cache";
+import { signIn, signOut } from "./auth";
 
 export const addPost = async (formData) => {
   // const title = formData.get("title");
@@ -43,4 +44,14 @@ export const deletePost = async (formData) => {
     console.log(err);
     return { error: "Something went wrong!" };
   }
+};
+
+export const handleGithubLogin = async () => {
+  "use server";
+  await signIn("github");
+};
+
+export const handleLogout = async () => {
+  "use server";
+  await signOut();
 };
