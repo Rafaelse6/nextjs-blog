26e00c509bf77713972f31a0e8dd74fd707a1ed1
Rafael Santos <rafa.05125@gmail.com>
2024-04-13 00:17:15 -0300

    Admin dashboard

--- blog-nextjs/src/lib/actions.js
+++ blog-nextjs/src/lib/actions.js
@@ -1,12 +1,12 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { Post, User } from "./models";
 import { connectToDb } from "./utils";
-import { revalidatePath } from "next/cache";
 import { signIn, signOut } from "./auth";
 import bcrypt from "bcryptjs";
 
-export const addPost = async (formData) => {
+export const addPost = async (prevState, formData) => {
   // const title = formData.get("title");
   // const desc = formData.get("desc");
   // const slug = formData.get("slug");
@@ -25,8 +25,9 @@ export const addPost = async (formData) => {
     await newPost.save();
     console.log("saved to db");
     revalidatePath("/blog");
-  } catch (error) {
-    console.log(error);
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
     return { error: "Something went wrong!" };
   }
 };
@@ -40,6 +41,43 @@ export const deletePost = async (formData) => {
     await Post.findByIdAndDelete(id);
     console.log("deleted from db");
     revalidatePath("/blog");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
+
+export const addUser = async (prevState, formData) => {
+  const { username, email, password, img } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+    const newUser = new User({
+      username,
+      email,
+      password,
+      img,
+    });
+
+    await newUser.save();
+    console.log("saved to db");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
+
+export const deleteUser = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+
+    await Post.deleteMany({ userId: id });
+    await User.findByIdAndDelete(id);
+    console.log("deleted from db");
     revalidatePath("/admin");
   } catch (err) {
     console.log(err);
